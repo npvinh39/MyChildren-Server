40fcd95cf06c60c468c4459a620617f5e2266604
Nguyen Phuc Vinh <nguyenphucvinh9x@gmail.com>
2023-10-17 10:36:17 +0700

    Updated cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
+const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
