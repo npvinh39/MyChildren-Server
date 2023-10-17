08c5dec6e17edd8612028cfbf4830b4979c4764b
Nguyen Phuc Vinh <nguyenphucvinh9x@gmail.com>
2023-10-17 10:33:21 +0700

    Updated policy cors

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 // import schedule
 const schedulePromotion = require('./middlewares/schedulePromotion');
 
